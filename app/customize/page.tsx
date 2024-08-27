3dd8d330e01c65f8f1294728b40dec03961d582b
VJL0 <jlvictor2020@gmail.com>
2024-08-26 23:39:27 -0400

    rewrote customize page

--- app/customize/page.tsx
+++ app/customize/page.tsx
@@ -1,12 +1,17 @@
-//import Dropdown from "@/components/Dropdown";
+import Dropdown from "@/components/Dropdown";
+import React from "react";
+
+interface Program {
+  name: string;
+}
 
 const CustomizePage = async () => {
   const response = await fetch(`${process.env.BASE_URL}/api/app`);
-  const programs = await response.json();
+  const programs: Program[] = await response.json();
 
   return (
     <div>
-      {programs}
+      <Dropdown programs={programs} />
     </div>
   );
 };
