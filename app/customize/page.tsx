6d6121ea003497ab7c539611b72efd8e9e7a70f7
VJL0 <jlvictor2020@gmail.com>
2024-08-26 22:07:21 -0400

    debugging api endpoint

--- app/customize/page.tsx
+++ app/customize/page.tsx
@@ -1,13 +1,15 @@
 //import Dropdown from "@/components/Dropdown";
 
 const CustomizePage = async () => {
+  const response = await fetch(`${process.env.BASE_URL}/api/app`);
+  const htmlContent = await response.text();
+
   //const response = await fetch(`${process.env.BASE_URL}/api/flask/getPrograms`);
   //const programs = await response.json();
 
   return (
     <div>
-      hello
-      {/* <Dropdown programs={programs} /> */}
+      {htmlContent}
     </div>
   );
 };
