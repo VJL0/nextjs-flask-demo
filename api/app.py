ad5ec1f40da8152cc566858f9a0a7e5e126608b1
VJL0 <jlvictor2020@gmail.com>
2024-08-26 23:12:19 -0400

    rewrite api endpointv6

--- api/app.py
+++ api/app.py
@@ -1,31 +1,12 @@
-from flask import Flask
-import requests
-from bs4 import BeautifulSoup
-
-def get_academic_programs()->list:
-    program_list = []
-    req = requests.get('https://bulletin.temple.edu/academic-programs/')
-    soup = BeautifulSoup(req.content, 'lxml')
-
-
-    # Find the body containing all the programs/degrees
-    main_body = soup.find('tbody', class_='fixedTH', id='degree_body')
-
-    # Iterate through each program in the body
-    for program_element in main_body.find_all('tr'):
-        program_children = program_element.find_all('td') # Children contain program name and degree types
-        program_name =  program_children[0].get_text(strip=True)
-        program_list.append(program_name)
-
-    return program_list
+from flask import Flask, jsonify
+from temple_requests import get_academic_programs
 
 app = Flask(__name__)
 
 @app.route("/api/app")
 def get_academic_programs_wrapper():
-    return get_academic_programs()
+    data = get_academic_programs()
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
