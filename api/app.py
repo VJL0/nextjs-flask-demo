fb88eb429bd89e9fe492623acbe9ddd6931477b5
VJL0 <jlvictor2020@gmail.com>
2024-08-26 22:44:18 -0400

    dev: test with import

--- api/app.py
+++ api/app.py
@@ -1,24 +1,5 @@
 from flask import Flask, jsonify
-import requests
-from bs4 import BeautifulSoup
-
-
-def get_academic_programs()->list:
-    program_list = []
-    req = requests.get('https://bulletin.temple.edu/academic-programs/')
-    soup = BeautifulSoup(req.content, 'lxml')
-
-
-    # Find the body containing all the programs/degrees
-    main_body = soup.find('tbody', class_='fixedTH', id='degree_body')
-
-    # Iterate through each program in the body
-    for program_element in main_body.find_all('tr'):
-        program_children = program_element.find_all('td') # Children contain program name and degree types
-        program_name =  program_children[0].get_text(strip=True)
-        program_list.append(program_name)
-
-    return program_list
+from temple_requests import get_academic_programs
 
 app = Flask(__name__)
 
