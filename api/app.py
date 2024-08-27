5c301190bad9ce97cf591e9fd85723a952923ce5
VJL0 <jlvictor2020@gmail.com>
2024-08-26 22:56:32 -0400

    rewrite api endpoint

--- api/app.py
+++ api/app.py
@@ -1,12 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
 from temple_requests import get_academic_programs
 
 app = Flask(__name__)
 
-@app.route("/api/app")
+@app.route("/api/getPrograms")
 def get_academic_programs_wrapper():
-    data = get_academic_programs()
-    return jsonify(data)
+    return get_academic_programs()
 
 if __name__ == "__main__":
     app.run(debug=True)
