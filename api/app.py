fc512a353f18b586b8f4e489c1493b84fe541aac
VJL0 <jlvictor2020@gmail.com>
2024-08-26 22:23:03 -0400

    jsonify data

--- api/app.py
+++ api/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 from temple_requests import get_academic_programs
 
 app = Flask(__name__)
 
 @app.route("/api/app")
 def get_academic_programs_wrapper():
-    return get_academic_programs()
+    data = get_academic_programs()
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
