fce3f133f0387a1c1477552a899fbbc0aec54ce7
VJL0 <jlvictor2020@gmail.com>
2024-08-26 23:42:53 -0400

    new route

--- app/gettingstarted/page.tsx
+++ app/gettingstarted/page.tsx
@@ -1,13 +1,9 @@
 import Dropdown from "@/components/Dropdown";
 import React from "react";
 
-interface Program {
-  name: string;
-}
-
 const CustomizePage = async () => {
   const response = await fetch(`${process.env.BASE_URL}/api/app`);
-  const programs: Program[] = await response.json();
+  const programs = await response.json();
 
   return (
     <div>
