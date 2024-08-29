a880710e2374acb82b8825d92d7889ebd20050dd
VJL0 <jlvictor2020@gmail.com>
2024-08-29 19:55:24 -0400

    test data

--- components/ProgramDropdown.tsx
+++ components/ProgramDropdown.tsx
@@ -1,19 +1,19 @@
 import Dropdown from "@/components/Dropdown";
 
-interface Program {
-  program: string;
-}
+// interface Program {
+//   program: string;
+// }
 
 const ProgramDropdown = async () => {
   // Fetch the programs data from the API
-  const response = await fetch(`${process.env.BASE_URL}/api/app`);
-  const data: Program[] = await response.json();
+  // const response = await fetch(`${process.env.BASE_URL}/api/app`);
+  // const data: Program[] = await response.json();
 
-  const programs = data.map((item) => item.program);
+  // const programs = data.map((item) => item.program);
 
   return (
     <div>
-      <Dropdown name={"Program"} data={programs} />
+      <Dropdown name={"Program"} data={["programs", "text", "data"]} />
     </div>
   );
 };
