3f86ca7fd71c35874715978ad2bfb5fa0f1dd6a6
VJL0 <jlvictor2020@gmail.com>
2024-08-29 19:50:36 -0400

    removed function from endpoint

--- api/app.py
+++ api/app.py
@@ -1,13 +1,46 @@
+from bs4 import BeautifulSoup
 from flask import Flask, jsonify
-from temple_requests import get_academic_programs
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/api/app")
 def get_academic_programs_wrapper():
-    data = get_academic_programs()
-    return jsonify(data)
+    program_list = []
+
+    try:
+        req = requests.get("https://bulletin.temple.edu/academic-programs/")
+        soup = BeautifulSoup(req.content, "lxml")
+
+        # The body containing all the programs/degrees
+        main_body = soup.find("tbody", class_="fixedTH", id="degree_body")
+
+        # Iterate through each program in the body
+        for program_element in main_body.find_all("tr"):
+            # Children contain program name and degree types
+            program_children = program_element.find_all("td")
+            program_name = program_children[0].get_text(strip=True)
+
+            for index in range(1, 4):
+                child = program_children[index]
+                program_degrees = child.get_text(strip=True)
+                program_link = child.find("a").get("href") if child.find("a") else ""
+
+                if program_degrees:
+                    degrees = program_degrees.split(",")
+                    for degree in degrees:
+                        program_list.append(
+                            {
+                                "program": f"{program_name} {degree.strip()}",
+                                "link": program_link,
+                            }
+                        )
+        return program_list
+
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+        return program_list
 
 
 if __name__ == "__main__":
