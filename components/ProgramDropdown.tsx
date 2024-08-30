0a37952af7fb4b69dbab80d80e08eece3ef91e21
VJL0 <jlvictor2020@gmail.com>
2024-08-29 20:24:16 -0400

    fix api call

--- components/ProgramDropdown.tsx
+++ components/ProgramDropdown.tsx
@@ -5,17 +5,26 @@ interface Program {
 }
 
 const ProgramDropdown = async () => {
-  // Fetch the programs data from the API
-  const response = await fetch("/api/app");
-  const data: Program[] = await response.json();
+  try {
+    // Fetch the programs data from the API
+    const url = new URL("/api/app", process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000");
+    const response = await fetch(url.toString());
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data: Program[] = await response.json();
 
-  const programs = data.map((item) => item.program);
+    const programs = data.map((item) => item.program);
 
-  return (
-    <div>
-      <Dropdown name={"Program"} data={programs} />
-    </div>
-  );
+    return (
+      <div>
+        <Dropdown name={"Program"} data={programs} />
+      </div>
+    );
+  } catch (error) {
+    console.error('Failed to fetch programs:', error);
+    return <div>Error loading programs</div>;
+  }
 };
 
 export default ProgramDropdown;
